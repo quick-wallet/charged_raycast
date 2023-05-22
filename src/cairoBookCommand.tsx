28d8ff1513316a7a1f00cbef91775c16c897928a
apoorveth <95699312+apoorveth@users.noreply.github.com>
2023-05-22 14:03:16 +0530

    removed unused variables, fixed titles and added screenshots

--- src/cairoBookCommand.tsx
+++ src/cairoBookCommand.tsx
@@ -1,5 +1,4 @@
 import { List, ActionPanel, Action } from "@raycast/api";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import fs from "fs";
 
@@ -36,7 +35,7 @@ export default function Command() {
           title={item.markdown.trim().split("\n")[0].replaceAll("#", "").trim()}
           detail={<List.Item.Detail markdown={item.markdown} />}
           actions={
-            <ActionPanel title="Open in browser">
+            <ActionPanel>
               <Action.OpenInBrowser url={`https://cairo-book.github.io/${item.name.replace(".md", ".html")}`} />
             </ActionPanel>
           }
